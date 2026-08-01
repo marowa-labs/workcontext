945225dd5ed86f60c582f3b9f88ac28c3ec18406
Marowa Labs <marowalabs@gmail.com>
2026-08-01 22:02:11 +0530

    Disable no-explicit-any rule in eslint.config.mjs

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -68,6 +68,17 @@ export default function RootLayout({
           data-form="fed3dc92-8dab-11f1-a702-9f06b570a5bb"
           strategy="afterInteractive"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L6YH94GFC1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-gtag" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-L6YH94GFC1');`}
+        </Script>
       </body>
     </html>
   );
